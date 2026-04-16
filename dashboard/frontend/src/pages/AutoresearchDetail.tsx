585d52cde01e03bb7ae8f8189a85330a8c486e30
Javier Chatruc <jrchatruc@gmail.com>
2026-04-16 18:36:42 -0300

    Fix Logs tab xterm sizing in autoresearch detail page

--- dashboard/frontend/src/pages/AutoresearchDetail.tsx
+++ dashboard/frontend/src/pages/AutoresearchDetail.tsx
@@ -252,7 +252,9 @@ export default function AutoresearchDetail() {
 
         {/* Logs tab */}
         <TabsContent value="logs" forceMount className="flex-1 flex flex-col min-h-0 rounded-b-lg border border-t-0 border-border bg-card overflow-hidden data-[state=inactive]:hidden">
-          <LogViewer autoresearchRunId={id!} />
+          <div className="flex-1 min-h-0">
+            <LogViewer autoresearchRunId={id!} />
+          </div>
         </TabsContent>
 
         {/* Config tab */}
